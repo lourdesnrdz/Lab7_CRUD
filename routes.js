9ff4307e1a627ffec4f3011c565b795e35a312f4
lourdesnrdz <lulynavarrete@hotmail.com>
2019-11-18 17:56:44 -0600

    update

--- routes.js
+++ routes.js
@@ -8,7 +8,7 @@ const router = express.Router()
 router.get('/persons', persons.getPersons)
 router.get('/persons/:id', persons.getPerson)
 router.post('/persons', persons.createPerson)
-router.patch('persons/:id', persons.updatePerson)
+router.patch('/persons/:id', persons.updatePerson)
 router.delete('/persons/:id', persons.deletePerson)
 
 module.exports = router
